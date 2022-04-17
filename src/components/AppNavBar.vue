7d0cdda542981e16dcfec27fdb5c65ee16363558
Antonio Gil <antonioluisgil@gmail.com>
2022-04-17 19:48:37 +0200

    style: add a container in the navbar

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -5,42 +5,46 @@ const showMobileMenu = ref(false);
 </script>
 
 <template>
-  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <RouterLink to="/" class="navbar-item is-size-4">Noteballs</RouterLink>
+  <nav class="navbar is-success" aria-label="main navigation" role="navigation">
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <RouterLink to="/" class="navbar-item is-size-4">Noteballs</RouterLink>
 
-      <a
-        @click.prevent="showMobileMenu = !showMobileMenu"
+        <a
+          :class="{ 'is-active': showMobileMenu }"
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="appNavbarMenu"
+          @click.prevent="showMobileMenu = !showMobileMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="appNavbarMenu"
+        class="navbar-menu"
         :class="{ 'is-active': showMobileMenu }"
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
       >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div
-      id="navbarBasicExample"
-      class="navbar-menu"
-      :class="{ 'is-active': showMobileMenu }"
-    >
-      <div class="navbar-end">
-        <RouterLink to="/" class="navbar-item">Notes</RouterLink>
-        <RouterLink to="/stats" class="navbar-item">Stats</RouterLink>
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item">Notes</RouterLink>
+          <RouterLink to="/stats" class="navbar-item">Stats</RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <style>
-.navbar-menu {
-  position: absolute;
-  left: 0;
-  width: 100%;
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
 }
 </style>
