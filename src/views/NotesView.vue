7e5fcf6195779f0b8c8ad1a132b6d66bbb397fb8
Antonio Gil <antonioluisgil@gmail.com>
2022-04-23 20:04:19 +0200

    feat: add method to create new note

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import type { Note } from '@/types/note';
+import { v4 as uuid } from 'uuid';
 import { ref } from 'vue';
+
+const newNoteRef = ref<HTMLTextAreaElement | null>(null);
+const newNote = ref('');
 
 const notes = ref<Note[]>([
   { id: 'id1', body: 'Note 1' },
@@ -8,6 +12,20 @@ const notes = ref<Note[]>([
   { id: 'id3', body: 'Note 3' },
   { id: 'id4', body: 'Note 4' },
 ]);
+
+function addNote() {
+  if (!newNote.value?.trim()) {
+    return;
+  }
+
+  notes.value.unshift({
+    id: uuid(),
+    body: newNote.value,
+  });
+
+  newNote.value = '';
+  newNoteRef.value?.focus();
+}
 </script>
 
 <template>
@@ -15,13 +33,26 @@ const notes = ref<Note[]>([
     <div class="has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add new note"></textarea>
+          <textarea
+            ref="newNoteRef"
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add new note"
+            @keyup.ctrl.enter="addNote"
+          ></textarea>
+          <p class="help is-white">
+            <span class="is-family-monospace">Ctrl + Enter</span> to add the
+            note
+          </p>
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            class="button is-link has-background-success"
+            @click="addNote"
+          >
             Add new note
           </button>
         </div>
